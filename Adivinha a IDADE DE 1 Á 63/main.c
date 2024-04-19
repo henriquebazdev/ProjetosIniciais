313a12e70bdf48854687819f6a6a76b8e6a50bc1
Henrique <h24henriquebaz@gmail.com>
2024-04-19 11:55:17 -0300

    Criei a função da primeira carta
    Criei a função de coleta e a função da primeira carta

--- Adivinha a IDADE DE 1 Á 63/main.c
+++ Adivinha a IDADE DE 1 Á 63/main.c
@@ -3,6 +3,72 @@
 #include <locale.h>
 #include <string.h>
 
+
+int primeira_carta()
+{
+	system("cls");
+	
+	int primeira_carta[8][4];
+	int l, c = 0;
+	int aux = 1;
+	for (l = 0; l < 8; l++)
+	{
+		for (c = 0; c < 4; c++)
+		{
+			primeira_carta[l][c] = aux;
+			aux+=2;
+			printf("[ %*d ]", 2, primeira_carta[l][c]);
+		}
+		printf("\n");
+	}
+	
+	int x = 0;
+	for (x = 0; x == 0;)
+	{
+		char opcao[1];
+		printf("O número pensado esta na primeira carta? 'S' ou 'N'\n");
+		printf("Opção: ");
+		scanf("%s", &opcao);
+	
+		if (strcmp(opcao, "S") == 0 || strcmp(opcao, "s") == 0)
+		{
+			system("cls");
+			printf("Ok! O número pensado esta nesta carta...\n");
+			return "S";
+			system("pause");
+		}
+		else if (strcmp(opcao, "N") == 0 || strcmp(opcao, "n") == 0)
+		{
+			system("cls");
+			printf("Ok! O número pensado não esta nesta carta...\n");
+			return "N";
+			system("pause");
+		}
+		else
+		{
+			system("cls");
+			printf("Opção inválida!\n");
+			printf("Tente novamente...\n");
+			system("pause");
+		}
+	}
+}
+
+
+int coleta()
+{
+	
+	int respostas[6];
+	int dado;
+	
+	dado = primeira_carta();
+	respostas[0] = dado;
+	printf("%s\n\n", respostas[0]);
+	
+	system("pause");
+	
+}
+
 int jogar()
 {
 	system("cls");
@@ -13,6 +79,8 @@ int jogar()
 	printf("4. Responda todas as perguntas com atenção pois não é possível voltar para corrigir uma resposta.\n\n");
 	printf("5. Ao final do jogo, o programa revelará o número que você pensou e o número total de perguntas feitas.\n\n");
 	system("pause");
+	
+	coleta();
 	
 	system("cls");
 }
